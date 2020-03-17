42c3bb5b04aac5a0e728dcdd0e86af6f4af5e56c
damianc <damianczapiewski@gmail.com>
2020-03-17 16:26:25 +0100

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = env => {
+	const { mode } = env;
+
+	const entryPoint = {
+		production: './src/index',
+		demo: './demo/index'
+	};
+
+	const outputPoint = {
+		production: ['prod', 'objql.js'],
+		demo: ['demo', 'app.js']
+	};
+
+	return {
+		entry: entryPoint[mode],
+		output: {
+			path: path.resolve(__dirname, outputPoint[mode][0]),
+			filename: outputPoint[mode][1],
+			library: 'ObjQL',
+			libraryTarget: 'umd'
+		}
+	};
+};
