8bb78ca11591e553dc99cb1ee55eca71f4dca873
damianc <damianczapiewski@gmail.com>
2020-03-22 21:08:06 +0100

    Add tests for check() function

---
+++ spec/check.spec.js
@@ -0,0 +1,35 @@
+const {default: ObjQL} = require('../prod/objql');
+
+describe('The check() method', () => {
+	it('should select people whose first result equals 4', () => {
+		const collection = ObjQL([
+			{name: 'Adam', results: [4, 5, 2, 3]},
+			{name: 'Mark', results: [2, 4, 4, 5]}
+		]);
+
+		const result = collection.where({
+			results: ObjQL.check(result => result[0] === 4)
+		});
+
+		expect(result).toEqual(
+			jasmine.arrayWithExactContents([
+				{name: 'Adam', results: [4, 5, 2, 3]}
+			])
+		);
+	});
+
+	it('should select no name', () => {
+		const collection = ObjQL([
+			{name: 'Adam', results: [4, 5, 2, 3]},
+			{name: 'Mark', results: [2, 4, 4, 5]}
+		]);
+
+		const result = collection.where({
+			results: ObjQL.check(result => result[0] === 5)
+		});
+
+		expect(result).toEqual(
+			jasmine.empty()
+		);
+	});
+});
